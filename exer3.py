a3fa38ff6021f0c96b8b35b5ec3050f8e7345715
gil80 <just.gil@gmail.com>
2017-12-02 14:33:36 +1100

    exercise 3

---
+++ exer3.py
@@ -0,0 +1,26 @@
+#  Encapsulate this code in a function named count, and generalize it so that it accepts the string and the letter as arguments.
+
+'''word = 'banana'
+count = 0
+for letter in word:
+    if word == 'a':
+        count = count + 1
+print(count)'''
+
+
+def letterCounter(word, letter):
+    count = 0
+    for letter in word:
+        if word == letter:
+            count = count + 1
+    return count
+
+
+word = input('Please type a word: ')
+try:
+    inputWord = str(word)
+except:
+    print('This is not a word')
+    exit()
+letter = input('Please type a letter: ')
+print(letterCounter)
